15b0a49d06a93f0b0b8840bfb9171eb9b6c8b3b2
Guillermo Muntaner <guillermomp87@gmail.com>
2018-09-26 22:35:59 +0200

    Added Example to git

---
+++ Example/Pods/Target Support Files/GLCodeReview/GLCodeReview-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_GLCodeReview : NSObject
+@end
+@implementation PodsDummy_GLCodeReview
+@end
---
+++ Example/Pods/Target Support Files/Pods-GLCodeReview_Example/Pods-GLCodeReview_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GLCodeReview_Example : NSObject
+@end
+@implementation PodsDummy_Pods_GLCodeReview_Example
+@end
